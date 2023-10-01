bdb2370945972de0e6a790cb8b6948a7de93b921
Radja Thaher <radjapolem@gmail.com>
2023-10-01 16:31:02 -0400

    a

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
 
 const { SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
@@ -13,14 +14,22 @@ module.exports = {
             info) {
             //TO DO: Validate user data
             //TO DO: Make sure  user does not already exist
-            //TODO: hash password and create an auth token
+            const user = await User.findOne({ username });
+            if (user) {
+                throw new UserInputError('Username is taken', {
+                    errors: {
+                        username: 'This username is taken'
+                    }
+                });
+            }
+            //hash password and create an auth token
             password = await bcrypt.hash(password, 12);
 
             const newUser = new User({
                 email,
                 username,
                 password,
-                createdAt: new Date().toISOString
+                createdAt: new Date().toISOString()
             });
             
             const res = await newUser.save();
