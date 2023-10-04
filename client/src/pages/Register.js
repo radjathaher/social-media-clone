9896e5e8026582d45aa20110c72aed23f024c5e7
Radja Thaher <radjapolem@gmail.com>
2023-10-04 19:23:15 -0400

    commit

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,24 +1,38 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { Button, Form } from 'semantic-ui-react';
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 
 
 export default function Register() {
+    const [errors, setErrors] = useState({});
     const [values, setValues] = useState({
         username: '',
         email: '',
         password: '',
         confirmPassword: ''
     });
+    const navigate = useNavigate();
     
     const onChange = (event) => {
         setValues({...values, [event.target.name]: event.target.value});
     }
 
     const [addUser, { loading }] = useMutation(REGISTER_USER, {
-        update(proxy, result) {
+        update(_, result) {
             console.log(result);
+            navigate('/');
+        },
+        onError(err) {
+            console.error(err); // Log the error object to the console
+            if (err.graphQLErrors[0] && err.graphQLErrors[0].extensions && err.graphQLErrors[0].extensions.exception) {
+                // Check if the error details exist before trying to access them
+                setErrors(err.graphQLErrors[0].extensions.exception.code);
+            } else {
+                // Provide a fallback error message if the error details are not available
+                setErrors({ general: "An unexpected error occurred" });
+            }
         },
         variables: values
         }
@@ -27,9 +41,7 @@ export default function Register() {
     const onSubmit = (event) => {
         event.preventDefault();
         addUser();
-    }
-
-    
+    }    
 
     return (
         <div className='form-container'>
@@ -41,6 +53,7 @@ export default function Register() {
                     name='username'
                     type='text'
                     value={values.username}
+                    error={errors.username ? true : false}
                     onChange={onChange} 
                 />
                 <Form.Input
@@ -49,6 +62,7 @@ export default function Register() {
                 name='email'
                 type='email'
                 value={values.email}
+                error={errors.email ? true : false}
                 onChange={onChange} 
                 />
                 <Form.Input
@@ -57,6 +71,7 @@ export default function Register() {
                 name='password'
                 type='password'
                 value={values.password}
+                error={errors.password ? true : false}
                 onChange={onChange} 
                 />
                 <Form.Input
@@ -65,16 +80,29 @@ export default function Register() {
                 name='confirmPassword'
                 type='password'
                 value={values.confirmPassword}
+                error={errors.confirmPassword ? true : false}
                 onChange={onChange} 
                 />
                 <Button type='submit' primary>
                     Register
                 </Button>
             </Form>
+            {Object.keys(errors).length > 0 && (
+            <div className='ui error message'>
+                <ul className='list'>
+                    {Object.values(errors).map(value => (
+                        <li key={value}>{value}</li>
+                    ))}
+                </ul>
+            </div>
+            )}
         </div>
     );
 };
 
+/*
+
+*/
 const REGISTER_USER = gql`
     mutation register(
         $username: String!
